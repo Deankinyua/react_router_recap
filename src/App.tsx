76e842491acf123ddd9ac3c2d80ece3837a88b3b
deankinyua <kinyuadean@gmail.com>
2024-06-11 05:49:41 +0300

    a bright morning

--- src/App.tsx
+++ src/App.tsx
@@ -10,12 +10,14 @@ import About from "./pages/About";
 import Home from "./pages/Home";
 
 // Layouts
+import RootLayout from "./layout/RootLayout";
 
 // Loaders
 
 const router = createBrowserRouter(
   createRoutesFromElements(
-    <Route path="/" element={<Home />}>
+    <Route path="/" element={<RootLayout />}>
+      <Route index element={<Home />}></Route>
       <Route path="/about" element={<About />}></Route>
     </Route>
   )
---
+++ src/layout/RootLayout.tsx
@@ -0,0 +1,22 @@
+import { NavLink, Outlet } from "react-router-dom";
+
+const RootLayout = () => {
+  return (
+    <div className="root-layout">
+      <header>
+        <nav>
+          <h1>Jobarouter</h1>
+          {/* NavLink is basically an anchor tag inbuilt into react */}
+          <NavLink to="/">Home</NavLink>
+          <NavLink to="about">About</NavLink>
+        </nav>
+      </header>
+      <main>
+        {/*  The outlet component allows us to render those component routes embedded in the root layout */}
+        <Outlet />
+      </main>
+    </div>
+  );
+};
+
+export default RootLayout;
