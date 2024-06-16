b99bb8f50c1d3e9a37645664653ecb8eaf582b21
deankinyua <kinyuadean@gmail.com>
2024-06-16 07:48:56 +0300

    back to react router

--- src/pages/careers/LoaderFunction.ts
+++ src/pages/careers/LoaderFunction.ts
@@ -1,4 +1,4 @@
-// * loader function
+// * loader function load data into a component before it renders.
 export const careersLoader = async () => {
   const res = await fetch("http://localhost:4000/careers");
   return res.json();
